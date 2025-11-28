9673e66f4e0043734ecc6041da3093a626f4d526
DrPepper18 <dbusygin70@gmail.com>
2025-11-29 01:02:01 +0300

    reworked during class

--- chapter-07/frontend.py
+++ chapter-07/frontend.py
@@ -1,16 +1,13 @@
 from fastapi import APIRouter, Request, Form, HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from backend import stack
 
 pages = APIRouter(prefix="")
-
-# Настраиваем шаблонизатор
 templates = Jinja2Templates(directory="templates")
 
-# Главная страница
 @pages.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse(
-        "index.html", {"request": request, "user": None}
+        "index.html", {"request": request, "user": "олег", "stack": stack}
     )
